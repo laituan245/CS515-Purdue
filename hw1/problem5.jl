c29f7d123b74e2099efb1adba7007184ca966643
Tuan Lai <tlai@adobe.com>
2019-08-30 12:21:39 -0400

    Done with part 4 of problem 5 hw1

--- hw1/problem5.jl
+++ hw1/problem5.jl
@@ -11,3 +11,22 @@ using LinearAlgebra
 part_2_answer = tr(X1+X2+X3) # this computed the trace
 println("The answer to the part 2 is:")
 println(part_2_answer)
+
+# Part 4
+function crude_edge_detector(nin,nout)
+  Nx = reshape(1:(nin*nin), nin, nin)
+  Ny = reshape(1:(nout*nout), nout, nout)
+  W = zeros(nout^2,nin^2)
+  for i=1:nin
+    for j=1:nin
+      xi = Nx[i, j]
+      yj = Ny[Int(ceil(i/2)), Int(ceil(j/2))]
+      is_1 = (((i % 2) + (j % 2)) % 2 == 0)
+      W[yj, xi] = is_1 ? 1 : -1
+    end
+  end
+  return W
+end
+
+W1 = crude_edge_detector(32,16)
+W2 = crude_edge_detector(16,8)
