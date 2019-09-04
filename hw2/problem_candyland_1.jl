9d4de9d44da1154cb5e07cd959f41d92814f79f6
Tuan Lai <tlai@adobe.com>
2019-09-04 17:03:06 -0400

    Added code to solve linear system (part 2 of question 1)

---
+++ hw2/problem_candyland_1.jl
@@ -0,0 +1,18 @@
+using LinearAlgebra
+using DelimitedFiles
+using SparseArrays
+
+# Input the transition probability matrix T
+data = readdlm("candyland-matrix.csv",',')
+TI = Int.(data[:,1])
+TJ = Int.(data[:,2])
+TV = data[:,3]
+T = sparse(TI,TJ, TV, 140,140)
+
+# Prepare the vector b
+b = ones((140, 1))
+b[134] = 0
+
+# Solve for x
+x =  (T' - I) \ -b
+println(x[140])
