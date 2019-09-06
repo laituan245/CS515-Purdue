a266405f7b75e55aba077208107a6281d6535901
Tuan Lai <tlai@adobe.com>
2019-09-06 19:49:53 -0400

    Added test for csc_transpose_matvec

--- hw2/problem3.jl
+++ hw2/problem3.jl
@@ -12,3 +12,31 @@ function csc_transpose_matvec(colptr, rowval, nzval, m, n, x)
   end
   return y
 end
+
+
+# Test cases
+using DelimitedFiles
+using SparseArrays
+data = readdlm("candyland-matrix.csv",',')
+TI = Int.(data[:,1])
+TJ = Int.(data[:,2])
+TV = data[:,3]
+T = sparse(TI,TJ, TV, 140,140)
+
+A = sprandn(4,5,0.5) # 4x5 matrix
+B = sprandn(10, 3, 0.8) # 10x3 matrix
+
+# Test the function csc_transpose_matvec
+x = randn(4)
+passed_test = A'*x == csc_transpose_matvec(A.colptr, A.rowval, A.nzval, A.m, A.n, x)
+print("Passed test 1? ")
+println(passed_test)
+println(A'*x)
+println(csc_transpose_matvec(A.colptr, A.rowval, A.nzval, A.m, A.n, x))
+
+x2 = rand(10)
+passed_test = B'*x2 == csc_transpose_matvec(B.colptr, B.rowval, B.nzval, B.m, B.n, x2)
+print("\nPassed test2? ")
+println(passed_test)
+println(B'*x2)
+println(csc_transpose_matvec(B.colptr, B.rowval, B.nzval, B.m, B.n, x2))
