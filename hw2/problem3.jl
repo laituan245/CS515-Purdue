9b459009a18154c00a3a4e00c84c244b7ae8ed2d
Tuan Lai <tlai@adobe.com>
2019-09-06 19:33:17 -0400

    Added initial code for problem 3.1 + Sparse matrix-transpose multiplication by a vector

---
+++ hw2/problem3.jl
@@ -0,0 +1,13 @@
+""" 1. Sparse matrix-transpose multiplication by a vector
+Returns y = A'*x where A is given by the CSC arrays
+colptr, rowval, nzval, m, n and x is the vector. """
+function csc_transpose_matvec(colptr, rowval, nzval, m, n, x)
+  y = zeros(n)
+  for j=1:length(colptr)-1 # for each column ...
+      for nzi=colptr[j]:colptr[j+1]-1 # for each entry in the column
+          i = rowval[nzi]
+          v = nzval[nzi]
+          y[j] += v*x[i]
+      end
+  end
+end
