77fcc992e8431d51f59d1b54a5cd22fa2af7e13a
Tuan Lai <tlai@adobe.com>
2019-09-06 23:54:42 -0400

    Done problem 1.3

--- hw2/problem_candyland_1.jl
+++ hw2/problem_candyland_1.jl
@@ -19,3 +19,24 @@ using SparseArrays
 # println(x[140])
 # println(x[47])
 # ----------------------------------------------------------------------------
+
+# ---------------------------------- PART 3 ----------------------------------
+# Input the transition probability matrix T
+data = readdlm("candyland-matrix.csv",',')
+TI = Int.(data[:,1])
+TJ = Int.(data[:,2])
+TV = data[:,3]
+T = sparse(TI,TJ, TV, 140,140)
+
+function calculate_expected_length(T)
+    p = T[:, 140]
+    expected_length, k = 0, 0
+    while norm(p) > 1e-18
+        k += 1
+        expected_length += k * p[134]
+        p = T * p
+    end
+    return expected_length
+end
+println(calculate_expected_length(T))
+# ----------------------------------------------------------------------------
