8028ec206244f4f405655b60ba755996255544df
Tuan Lai <tlai@adobe.com>
2019-09-08 01:54:40 -0400

    Probably done with problem 2_2

--- hw2/problem_2_2.jl
+++ hw2/problem_2_2.jl
@@ -1,3 +1,5 @@
+using SparseArrays
+
 function laplacian(n::Integer, f::Function)
     N = (n+1)^2
     nz = (n+1) * (5*n+1)
@@ -16,12 +18,29 @@ function laplacian(n::Integer, f::Function)
                 # we are on a boudnary
                 fvec[row] = 0.0
                 # fill in entries in I,J,V and update index
+                I[index] = row; J[index] = row; V[index]=-4*n^2; index+=1
+                if j > 0 I[index] = row; J[index] = row-1; V[index]=n^2; index+=1 end
+                if j < n I[index] = row; J[index] = row+1; V[index]=n^2; index+=1 end
+                if i > 0 I[index] = row; J[index] = row-n-1; V[index]=n^2; index+=1 end
+                if i < n I[index] = row; J[index] = row+n+1; V[index]=n^2; index+=1 end
             else
                 fvec[row] = f(i*h, j*h)*h^2
                 # fill in entries in I,J,V and update index
+                I[index] = row; J[index] = row; V[index]=-4*n^2; index+=1
+                I[index] = row; J[index] = row-1; V[index]=n^2; index+=1
+                I[index] = row; J[index] = row+1; V[index]=n^2; index+=1
+                I[index] = row; J[index] = row-n-1; V[index]=n^2; index+=1
+                I[index] = row; J[index] = row+n+1; V[index]=n^2; index+=1
             end
         end
     end
     A = sparse(I,J,V,N,N)
     return A, fvec
 end
+
+function f(x, y)
+    return 1
+end
+
+A, fvec = laplacian(10, f)
+uvec = A \ fvec
