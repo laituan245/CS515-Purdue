18f14b593ad799a01b8b06fd1352d80568d8e823
Tuan Lai <tlai@adobe.com>
2019-09-22 18:22:07 -0400

    Added the initial code for candyland from hw2

---
+++ hw4/candyland.jl
@@ -0,0 +1,20 @@
+using LinearAlgebra
+using DelimitedFiles
+using SparseArrays
+
+# ---------------------------------- PART 2 ----------------------------------
+# Input the transition probability matrix T
+data = readdlm("candyland-matrix.csv",',')
+TI = Int.(data[:,1])
+TJ = Int.(data[:,2])
+TV = data[:,3]
+T = sparse(TI,TJ, TV, 140,140)
+
+# Prepare the vector b
+b = ones((140, 1))
+b[134] = 0
+
+# Solve for correct x
+x =  (T' - I) \ -b
+println(x[140])
+println(x[47])
