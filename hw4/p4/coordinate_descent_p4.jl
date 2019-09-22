1a884b2f93bf394693785be5f68e9d5d0eba7a08
Tuan Lai <tlai@adobe.com>
2019-09-22 14:53:20 -0400

    Added copies for problem4 of hw4

---
+++ hw4/p4/coordinate_descent_p4.jl
@@ -0,0 +1,107 @@
+using LinearAlgebra
+using SparseArrays
+
+function update_g(old_g, A, A_diagonal, index)
+  # old_g is the gradient vector from previous iteration
+  # A is a CSC matrix
+  # A_diagonal is a vector containing the values of the diagonal of A
+  # index is the selected coordinate
+  new_g = old_g
+  colptr = A.colptr; rowval = A.rowval; nzval = A.nzval
+  scalar = old_g[index] / A_diagonal[index]
+  for nzi=colptr[index]:colptr[index+1]-1
+      new_g[rowval[nzi]] -= (scalar * nzval[nzi])
+  end
+  return new_g
+end
+
+function extract_diagonal(A)
+    # Compute A_diagonal
+    A_diagonal = zeros(A.m)
+    for j=1:length(A.colptr)-1 # for each column ...
+        for nzi=A.colptr[j]:A.colptr[j+1]-1 # for each entry in the column
+            i = A.rowval[nzi]
+            v = A.nzval[nzi]
+            if i == j
+                A_diagonal[i] = v
+            end
+        end
+    end
+    return A_diagonal
+end
+
+## Poisson Equation
+function laplacian(n::Integer, f::Function)
+    N = (n+1)^2
+    nz = (n+1) * (5*n+1)
+    I = zeros(Int,nz)
+    J = zeros(Int,nz)
+    V = zeros(nz)
+    fvec = zeros(N)
+    # the transpose mirrors the row indexing we had before.
+    G = reshape(1:N, n+1, n+1)' # index map, like we saw before;
+    h = 1.0/(n)
+    index = 1
+    for i=0:n
+        for j=0:n
+            row = G[i+1,j+1]
+            if i==0 || j == 0 || i == n || j == n
+                # we are on a boudnary
+                fvec[row] = 0.0
+                # fill in entries in I,J,V and update index
+                I[index] = row; J[index] = row; V[index]=-4; index+=1
+                if j > 0 I[index] = row; J[index] = row-1; V[index]=1; index+=1 end
+                if j < n I[index] = row; J[index] = row+1; V[index]=1; index+=1 end
+                if i > 0 I[index] = row; J[index] = row-n-1; V[index]=1; index+=1 end
+                if i < n I[index] = row; J[index] = row+n+1; V[index]=1; index+=1 end
+            else
+                fvec[row] = f(i*h, j*h)*h^2
+                # fill in entries in I,J,V and update index
+                I[index] = row; J[index] = row; V[index]=-4; index+=1
+                I[index] = row; J[index] = row-1; V[index]=1; index+=1
+                I[index] = row; J[index] = row+1; V[index]=1; index+=1
+                I[index] = row; J[index] = row-n-1; V[index]=1; index+=1
+                I[index] = row; J[index] = row+n+1; V[index]=1; index+=1
+            end
+        end
+    end
+    A = sparse(I,J,V,N,N)
+    return A, fvec
+end
+
+function f(x, y)
+    return 1
+end
+
+A, fvec = laplacian(10, f)
+A, fvec = -A, -fvec
+
+function cyclic_coordinate_descent(A, b)
+    A_diagonal = extract_diagonal(A)
+    x = rand(A.n)
+    g = A * x - b
+    i = 1
+    while norm(g) > 1e-16
+        x[i] = x[i] - (g[i] / A_diagonal[i])
+        g = update_g(g, A, A_diagonal, i)
+        if (i == A.n) i = 1 else i += 1 end
+    end
+    return x
+end
+
+function random_coordinate_descent(A, b)
+    A_diagonal = extract_diagonal(A)
+    x = rand(A.n)
+    g = A * x - b
+    while norm(g) > 1e-16
+        i = rand(1:A.n)
+        x[i] = x[i] - (g[i] / A_diagonal[i])
+        g = update_g(g, A, A_diagonal, i)
+    end
+    return x
+end
+
+sol = random_coordinate_descent(A, fvec)
+
+uvec = A \ fvec
+println(norm(sol-uvec))
---
+++ hw4/p4/gradient_descent_p4.jl
@@ -0,0 +1,68 @@
+using LinearAlgebra
+using SparseArrays
+
+## Poisson Equation
+function laplacian(n::Integer, f::Function)
+    N = (n+1)^2
+    nz = (n+1) * (5*n+1)
+    I = zeros(Int,nz)
+    J = zeros(Int,nz)
+    V = zeros(nz)
+    fvec = zeros(N)
+    # the transpose mirrors the row indexing we had before.
+    G = reshape(1:N, n+1, n+1)' # index map, like we saw before;
+    h = 1.0/(n)
+    index = 1
+    for i=0:n
+        for j=0:n
+            row = G[i+1,j+1]
+            if i==0 || j == 0 || i == n || j == n
+                # we are on a boudnary
+                fvec[row] = 0.0
+                # fill in entries in I,J,V and update index
+                I[index] = row; J[index] = row; V[index]=-4; index+=1
+                if j > 0 I[index] = row; J[index] = row-1; V[index]=1; index+=1 end
+                if j < n I[index] = row; J[index] = row+1; V[index]=1; index+=1 end
+                if i > 0 I[index] = row; J[index] = row-n-1; V[index]=1; index+=1 end
+                if i < n I[index] = row; J[index] = row+n+1; V[index]=1; index+=1 end
+            else
+                fvec[row] = f(i*h, j*h)*h^2
+                # fill in entries in I,J,V and update index
+                I[index] = row; J[index] = row; V[index]=-4; index+=1
+                I[index] = row; J[index] = row-1; V[index]=1; index+=1
+                I[index] = row; J[index] = row+1; V[index]=1; index+=1
+                I[index] = row; J[index] = row-n-1; V[index]=1; index+=1
+                I[index] = row; J[index] = row+n+1; V[index]=1; index+=1
+            end
+        end
+    end
+    A = sparse(I,J,V,N,N)
+    return A, fvec
+end
+
+function f(x, y)
+    return 1
+end
+
+A, fvec = laplacian(10, f)
+A, fvec = -A, -fvec
+
+function gradient_descent(A, b)
+    x = rand(A.n)
+    g = A * x - b
+    Ag = A * g
+    while norm(g) > 1e-16
+        alpha = dot(g, g) / (dot(g, Ag))
+        x = x - alpha * g
+
+        # Update g and Ag
+        # The order of the following two statements is important
+        g = g - alpha * Ag
+        Ag = A * g
+    end
+    return x
+end
+
+sol = gradient_descent(A, fvec)
+uvec = A \ fvec
+println(norm(sol-uvec))
