569b7033efebe0cc46a169ed6a06563ce60b6d32
Tuan Lai <tlai@adobe.com>
2019-09-29 15:56:58 -0400

    Implemented Gauss-Seidel

--- hw5/problem2/problem2_gauss_seidel.jl
+++ hw5/problem2/problem2_gauss_seidel.jl
@@ -26,6 +26,47 @@ function extract_diagonal(A)
     return D, D_inverse
 end
 
+function extract_L_and_U(A)
+    # Compute L (the strict lower part) and U (the strict upper part) of A
+    U_I, U_J, U_V = Int64[], Int64[], Float64[]
+    L_I, L_J, L_V = Int64[], Int64[], Float64[]
+    for j=1:length(A.colptr)-1 # for each column ...
+        for nzi=A.colptr[j]:A.colptr[j+1]-1 # for each entry in the column
+            i = A.rowval[nzi]
+            v = A.nzval[nzi]
+            if j > i
+                push!(U_I, i)
+                push!(U_J, j)
+                push!(U_V, v)
+            end
+            if i > j
+                push!(L_I, i)
+                push!(L_J, j)
+                push!(L_V, v)
+            end
+        end
+    end
+
+    L = sparse(L_I, L_J, L_V, A.n, A.n)
+    U = sparse(U_I, U_J, U_V, A.n, A.n)
+    return L, U
+end
+
+function sparse_transpose(A)
+    # Compute A^T
+    I, J, V = Int64[], Int64[], Float64[]
+    for j=1:length(A.colptr)-1 # for each column ...
+        for nzi=A.colptr[j]:A.colptr[j+1]-1 # for each entry in the column
+            i = A.rowval[nzi]
+            v = A.nzval[nzi]
+            push!(I, j)
+            push!(J, i)
+            push!(V, v)
+        end
+    end
+    return sparse(I, J, V, A.n, A.n)
+end
+
 function csc_matvec(A, x)
     # Compute the matrix-vector product Ax
       y = zeros(A.n)
@@ -55,4 +96,33 @@ x =  (T' - I) \ -b
 
 # ========================= Gauss-Seidel Implementation =========================
 function gauss_seidel_method(P, A, b)
+    # A is a CSC matrix
+    # b is a vector
+    A, b = P * A, P * b
+
+    D, D_inverse = extract_diagonal(A)
+    L, U = extract_L_and_U(A)
+
+    c1 = D_inverse * b
+    c2 = D_inverse * U
+    c3 = sparse_transpose(D_inverse * L)
+
+    iterations = 0
+    x = rand(A.n)
+    while norm(csc_matvec(A, x) - b) / norm(b) > 1e-4
+        h_k = c1 - csc_matvec(c2, x)
+        for i in 1:A.n
+            x[i] = h_k[i]
+            for nzi=c3.colptr[i]:c3.colptr[i+1]-1 # for each entry in the column
+                x[i] -= x[c3.rowval[nzi]] * c3.nzval[nzi]
+            end
+        end
+        iterations += 1
+    end
+    return x, iterations
 end
+
+gauss_seidel_x, gauss_seidel_iterations = gauss_seidel_method(I, T'-I, -b)
+println(gauss_seidel_x[140])
+println(gauss_seidel_x[47])
+println(gauss_seidel_iterations)
