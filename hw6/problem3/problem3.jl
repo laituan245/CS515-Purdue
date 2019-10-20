7a46d95a5f7d0f1db0fc14b4a76848a4255adf08
Tuan Lai <tlai@adobe.com>
2019-10-20 12:52:44 -0400

    Extracted even numbered images

--- hw6/problem3/problem3.jl
+++ hw6/problem3/problem3.jl
@@ -4,3 +4,7 @@ using DelimitedFiles
 # Load the Yale face database of 165 pictures
 A = readdlm("Yale_64.csv",',')
 labels = readdlm("Yale_64_ids.csv")
+
+# Extract even numbered images
+nb_images = size(A)[1]
+even_images = A[2:2:nb_images, :]
