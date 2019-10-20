eae02695e36630fd269891cb85a004cd857738e9
Tuan Lai <tlai@adobe.com>
2019-10-20 13:25:51 -0400

    Added code to do normalization + least squares

--- hw6/problem3/problem3.jl
+++ hw6/problem3/problem3.jl
@@ -1,10 +1,24 @@
 using Plots
+using LinearAlgebra
 using DelimitedFiles
 
 # Load the Yale face database of 165 pictures
 A = readdlm("Yale_64.csv",',')
 labels = readdlm("Yale_64_ids.csv")
+nb_images, nb_features = size(A)
 
-# Extract even numbered images
-nb_images = size(A)[1]
+# Nomalize each vector to unit
+for i = 1:nb_images
+    A[i,:] = A[i,:] ./ max(1e-12,norm(A[i,:]));
+end
+
+# Let's represent each odd-numbered image in terms of the even-numbered images
+# in a least squares
 even_images = A[2:2:nb_images, :]
+X = even_images'
+for i = 1:2:nb_images
+    b = A[i, :] # the current odd-numbered image
+    xhat = X \ b
+    reconstruction_loss = norm(b - (X * xhat))
+    println(reconstruction_loss)
+end
