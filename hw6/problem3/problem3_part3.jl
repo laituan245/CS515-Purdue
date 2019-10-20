1950f7e13124253dcb1653dee79ac887cd6d603a
Tuan Lai <tlai@adobe.com>
2019-10-20 15:07:59 -0400

    Finished problem3_part3.jl

--- hw6/problem3/problem3_part3.jl
+++ hw6/problem3/problem3_part3.jl
@@ -1,8 +1,10 @@
 using Plots
+using Random
 using LinearAlgebra
 using DelimitedFiles
 
-function main()
+# Read and preprocess the Yale face database
+function read_and_preprocess()
     # Load the Yale face database of 165 pictures
     A = readdlm("Yale_64.csv",',')
     labels = readdlm("Yale_64_ids.csv")
@@ -13,28 +15,44 @@ function main()
         A[i,:] = A[i,:] ./ max(1e-12,norm(A[i,:]));
     end
 
-    # Let's represent each odd-numbered image in terms of the even-numbered images
-    # in a least squares
-    even_images = A[2:2:nb_images, :]
-    D = even_images'
-    min_reconstruction_loss, min_idx = Inf, nothing
-    max_reconstruction_loss, max_idx = -Inf, nothing
-    for i = 1:2:nb_images
-        b = A[i, :] # the current odd-numbered image
-        xhat = D \ b
-        reconstruction_loss = norm(b - (D * xhat))
-        if reconstruction_loss < min_reconstruction_loss
-            min_reconstruction_loss = reconstruction_loss
-            min_idx = i
-        end
-        if reconstruction_loss > max_reconstruction_loss
-            max_reconstruction_loss = reconstruction_loss
-            max_idx = i
-        end
-    end
-
-    println(min_idx)
-    println(max_idx)
+    return A, labels
 end
 
-main()
+# Find the value of sigma that will make face search fail for a particular image
+function find_sigma(A, labels, image_idx)
+    nb_images, nb_features = size(A)
+
+    # We use all images as the database D
+    D = A'
+
+    # Extract the image of interest
+    target_image = A[image_idx, :]
+
+    # Search for gamma
+    gamma = 0
+    while true
+        nb_fails = 0
+        for i = 1:100
+            preturbed_image = target_image + gamma * randn(nb_features)
+            xhat = D \ preturbed_image
+            if argmax(xhat) != image_idx
+                nb_fails += 1
+            end
+        end
+        if nb_fails >= 25
+            return gamma
+        end
+        gamma += 0.0001
+    end
+
+end
+
+A, labels = read_and_preprocess()
+
+# How large does sigma need to be before we won't recognize image 67 as image 67 anymore?
+sigma_for_67 = find_sigma(A, labels, 67)
+println(string("For image 67, sigma should be at least ", sigma_for_67))
+
+# How large does sigma need to be before we won't recognize image 7 anymore?
+sigma_for_7 = find_sigma(A, labels, 7)
+println(string("For image 7, sigma should be at least ", sigma_for_7))
