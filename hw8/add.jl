e219972e7b94c4ce54fd76d4faaaafbf4edc88a5
Tuan Lai <tlai@adobe.com>
2019-11-03 16:56:14 -0500

    Added function sorted_mysum

--- hw8/add.jl
+++ hw8/add.jl
@@ -6,4 +6,13 @@ function mysum(x::Vector{Float64})
   return s
 end
 
-println(mysum([5.3, 1.25, 3.0]))
+function sorted_mysum(x::Vector{Float64})
+  x = sort(x)
+  s = zero(Float64)
+  for i=1:length(x)
+    s += x[i]
+  end
+  return s
+end
+
+println(sorted_mysum([5.3, 1.25, 3.0]))
