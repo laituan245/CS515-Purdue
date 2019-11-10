369c7f3659ed41b92221220a2e123e6ada586b18
Tuan Lai <tlai@adobe.com>
2019-11-10 17:55:58 -0500

    for plots

--- hw9/problem3.jl
+++ hw9/problem3.jl
@@ -28,10 +28,34 @@ function lanczos_tri(A,b,k)
   return T
 end
 
-A = randn(10,10)
-A = A + A'
-b = randn(10)
-S = lanczos_tri(A,b,4)
-S = convert(Matrix, S)
-println(eigmax(S))
-println(eigmax(A))
+function power_method(A, b, niter)
+  n = size(A,1)
+  x = b
+  lam = 0.0
+  for i=1:niter
+    y = A*x
+    lam = y'*x
+    x = y./norm(y)
+  end
+  return lam
+end
+
+# 50 x 50 instance
+A = randn(50,50)
+A = A' * A
+b = randn(50)
+true_max_eig = eigmax(A)
+
+for niter=1:50
+
+    S = lanczos_tri(A,b,niter)
+    S = convert(Matrix, S)
+    krylov_method_eig = eigmax(S)
+    power_method_eig = power_method(A, b, niter)
+
+    krylov_error = abs(krylov_method_eig-true_max_eig)
+    power_method_eig = abs(power_method_eig-true_max_eig)
+
+    println(krylov_error)
+    println(power_method_eig)
+end
