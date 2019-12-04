54acba07fb74b0adf8e96a082e53be804e1d8332
Tuan Lai <tlai@adobe.com>
2019-12-04 12:03:19 -0500

    Added initial code for loading the data files

---
+++ hw11/Problem1/main.jl
@@ -0,0 +1,6 @@
+using DelimitedFiles
+using SparseArrays
+data = readdlm("poisson2D-data.csv",',')
+A = sparse(Int.(data[:,1]), Int.(data[:,2]),(data[:,3]))
+A = (A + A')./2
+b = vec(readdlm("poisson2D-rhs.csv"))
