cf606e27ed6593e51817a5e09f542f346885644d
Tuan Lai <tlai@adobe.com>
2019-12-04 12:15:26 -0500

    Added code to do MINRES and GMRES

--- hw11/Problem1/main.jl
+++ hw11/Problem1/main.jl
@@ -1,8 +1,15 @@
 using DelimitedFiles
+using LinearAlgebra
 using SparseArrays
+using IterativeSolvers
 
 # Load data
 data = readdlm("poisson2D-data.csv",',')
 A = sparse(Int.(data[:,1]), Int.(data[:,2]),(data[:,3]))
 A = (A + A')./2
 b = vec(readdlm("poisson2D-rhs.csv"))
+
+# Do MINRES and GMRES
+sigma = 1.7 * 1e-2
+x, hist_min = minres(A - sigma * I, b, tol = 1e-6, maxiter = 10000, log=true)
+x, hist_gm = gmres(A - sigma * I, b, restart = 30, maxiter = 10000, tol = 1e-6, log=true)
