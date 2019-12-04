d8ff308cd6a7ae12d304efc9eb54e4a9869cf769
Tuan Lai <tlai@adobe.com>
2019-12-04 14:16:14 -0500

    Defined a matrix C in part2.jl

--- hw11/Problem1/part2.jl
+++ hw11/Problem1/part2.jl
@@ -10,3 +10,7 @@ data = readdlm("poisson2D-data.csv",',')
 A = sparse(Int.(data[:,1]), Int.(data[:,2]),(data[:,3]))
 A = (A + A')./2
 b = vec(readdlm("poisson2D-rhs.csv"))
+
+# Define matrix C
+sigma = 1.7 * 1e-2
+C = A - sigma * I
