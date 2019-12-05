c84d49f31ab52ecfae13a34f1a00c44a6b6db6d0
Tuan Lai <tlai@adobe.com>
2019-12-04 22:42:55 -0500

    Done with part4

--- hw11/Problem2/part4.jl
+++ hw11/Problem2/part4.jl
@@ -14,3 +14,26 @@ function apply_poisson_gauss_seidel(X,B)
   end
   return Y
 end
+
+# Implement code to analyze the error trend
+function analyze_error_trend(n, iters=1500)
+    errors = zeros(iters)
+    F = poisson_setup(n,n, (x,y) -> 1)
+    X_correct = solve_poisson_direct(F)
+
+    X_gauss_seidel = zeros(size(F)...)
+    for i=1:iters
+        X_gauss_seidel = apply_poisson_gauss_seidel(X_gauss_seidel, F)
+        errors[i] = norm(X_gauss_seidel - X_correct) / norm(X_correct)
+    end
+
+    ratios = zeros(iters)
+    for i=2:iters
+        ratios[i] = errors[i] / errors[i-1]
+    end
+    savefig(plot(2:iters, ratios[2:iters], linewidth=2, label="Error Ratio"), "gauss_seidel_error_trend.png")
+    println(string("last ratio = ", ratios[iters]))
+    println(string("last error = ", errors[iters]))
+
+end
+analyze_error_trend(31)
