ddddece5645c5c353100904e0befc8a7df5d605d
Tuan Lai <tlai@adobe.com>
2019-12-06 18:50:30 -0500

    Done with part6

--- hw11/Problem2/part6.jl
+++ hw11/Problem2/part6.jl
@@ -13,3 +13,27 @@ X_63_true = solve_poisson_direct(poisson_setup(63,63, (x,y) -> 1))
 
 # Interpolate the solution from n = 31 to a n = 63 solution
 X_63_interpolate = interpolate(X_31_true)
+
+# Evaluate the error
+error = norm(X_63_interpolate - X_63_true)
+println(string("The error is ", error))
+
+# Make a mesh-plot
+function make_mesh_plot(mat)
+    m, n = size(mat)
+    x = zeros(m * n); y = zeros(m * n); z = zeros(m * n);
+    index = 1;
+    for i=1:m
+        for j=1:n
+            x[index] = i
+            y[index] = j
+            z[index] = mat[i,j]
+            index += 1
+        end
+    end
+    return surface(x, y, z)
+end
+
+diff = abs.(X_63_interpolate - X_63_true)
+pyplot()
+savefig(make_mesh_plot(diff), "mesh_plot.png")
