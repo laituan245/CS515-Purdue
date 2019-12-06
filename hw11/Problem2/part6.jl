f2d68d72086911bc4b44a19956a1e05f4a2e8a90
Tuan Lai <tlai@adobe.com>
2019-12-06 18:12:50 -0500

    Conduct interpolation

--- hw11/Problem2/part6.jl
+++ hw11/Problem2/part6.jl
@@ -10,3 +10,6 @@ X_31_true = solve_poisson_direct(poisson_setup(31,31, (x,y) -> 1))
 
 # Directly solving a Poisson problem where n = 63
 X_63_true = solve_poisson_direct(poisson_setup(63,63, (x,y) -> 1))
+
+# Interpolate the solution from n = 31 to a n = 63 solution
+X_63_interpolate = interpolate(X_31_true)
